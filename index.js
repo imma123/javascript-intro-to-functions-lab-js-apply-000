8064540b4fd986b226c995ab4fc8814481100fc8
Miriam Friedler <friedleram@gmail.com>
2019-02-15 17:56:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ function sayHiToGrandma(string){
     return "YES INDEED!"
   }
     else {
-      if(string ==== "I love you, Grandma."){
+      if(string === "I love you, Grandma."){
         return "I love you, too."
       }
     }
 }
+  
